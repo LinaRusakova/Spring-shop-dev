0938c6b801f8ad6098d06a9848b3e2def8c64874
Lina Rusakova <xlinaris@gmail.com>
2022-01-02 22:20:28 +0300

    added basic settings and initial liquibase script

--- src/main/java/com/gmail/xlinaris/springworkspace/Shop/frontend/MainView.java
+++ src/main/java/com/gmail/xlinaris/springworkspace/Shop/frontend/MainView.java
@@ -1,14 +1,22 @@
 package com.gmail.xlinaris.springworkspace.Shop.frontend;
 
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("main")
 public class MainView extends VerticalLayout {
     public MainView() {
+        var titleHL = new HorizontalLayout();
+        var textHL = new HorizontalLayout();
         var title=new Text("Title");
+        var titleButton = new Button("Кнопка", buttonClickEvent -> title.setText("кнопка нажата"));
         var text=new Text("some text");
-        add(title, text);
+
+        titleHL.add(title, titleButton);
+        textHL.add(text);
+        add(titleHL, textHL);
     }
 }
